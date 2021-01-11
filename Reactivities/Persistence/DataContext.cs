7f3d6def37b54f7af0806c882d8321b30642ab27
Ziyi Zhang <zhangziyibupt@gmail.com>
2021-01-10 17:35:54 -0800

    Create a Domain entity and add DB migration

---
+++ Reactivities/Persistence/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        // a table name in DB
+        public DbSet<Value> Values { get; set; }
+    }
+}
--- Reactivities/README.md
+++ Reactivities/README.md
@@ -1,7 +1,13 @@
 ```
-create project
+Create a project
 dotnet sln add API
 
 Add Domain as dependency
 dotnet add reference ../Domain
+
+Add entity framework tool
+dotnet tool install --global dotnet-ef
+
+Create a DB migration
+dotnet ef migrations add InitialCreate -p Persistence/ -s API/
 ```
