76114f2210582a6586ab46d8111c25bd0e435a15
Ziyi Zhang <zhangziyibupt@gmail.com>
2021-01-11 02:00:43 -0800

    Read from DB

---
+++ Reactivities/API/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        // inject data context to the controller
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+
+        // GET api/values
+        // async operation of DB access
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Value>>> Get()
+        {
+            var values = await _context.Values.ToListAsync();
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        // async operation of DB access
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Value>> Get(int id)
+        {
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
+        }
+    }
+}
--- Reactivities/README.md
+++ Reactivities/README.md
@@ -10,4 +10,5 @@ dotnet tool install --global dotnet-ef
 
 Create a DB migration
 dotnet ef migrations add InitialCreate -p Persistence/ -s API/
+dotnet ef migrations add SeedValues -p Persistence/ -s API/
 ```
