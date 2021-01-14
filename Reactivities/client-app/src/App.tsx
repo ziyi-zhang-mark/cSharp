d58c936ea7fa82a79a19426d94a8b934f9f464a5
Ziyi Zhang <zhangziyibupt@gmail.com>
2021-01-14 01:07:07 -0800

    typescript basics

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,26 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { cars } from "./demo";
+import { CarItem } from "./CarItem";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {cars.map((car) => (
+          <CarItem car={car} />
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
