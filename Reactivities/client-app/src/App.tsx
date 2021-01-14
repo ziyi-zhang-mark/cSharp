6cd62f602ffff42c079939ebc558d8c619a4b9a2
Ziyi Zhang <zhangziyibupt@gmail.com>
2021-01-14 01:11:20 -0800

    Revert "typescript basics"
    This reverts commit d58c936ea7fa82a79a19426d94a8b934f9f464a5.

--- Reactivities/client-app/src/App.tsx
+++ Reactivities/client-app/src/App.tsx
@@ -1,18 +1,26 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import { cars } from "./demo";
-import { CarItem } from "./CarItem";
 
-const App: React.FC = () => {
+function App() {
   return (
     <div className="App">
-      <ul>
-        {cars.map((car) => (
-          <CarItem car={car} />
-        ))}
-      </ul>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
