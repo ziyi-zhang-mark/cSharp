b9ff7f8a5bc49155b03e064021f8aa4aa0351b2d
Ziyi Zhang <zhangziyibupt@gmail.com>
2021-01-18 12:37:44 -0800

    Add Activity Details component

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Grid, List } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
+import ActivityDetails from "../details/ActivityDetails";
 import ActivityList from "./ActivityList";
 
 interface IProps {
@@ -12,6 +13,10 @@ const ActivityDashboard: React.FC<IProps> = ({ activities }) => {
     <Grid>
       <Grid.Column width={10}>
         <ActivityList activities={activities}></ActivityList>
+      </Grid.Column>
+
+      <Grid.Column width={6}>
+        <ActivityDetails></ActivityDetails>
       </Grid.Column>
     </Grid>
   );
